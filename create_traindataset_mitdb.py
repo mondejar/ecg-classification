9fa83d4b74b31d7046728f519d73560543bbcf10
mondejar <victormondejar@gmail.com>
2017-05-23 19:53:31 +0200

    23

--- create_traindataset_mitdb.py
+++ create_traindataset_mitdb.py
@@ -13,8 +13,6 @@ import matplotlib.pyplot as plt
 import os
 import csv
 import pickle
-import collections
-from collections import namedtuple
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -34,7 +32,7 @@ class mit_data:
         self.signals = []
         self.classes = []   
         self.selected_R = []       
-        self.temp_features = []       
+        self.temporal_features = []       
         self.window_size = []       
             
 dataset_path = '/local/scratch/mondejar/ECG/dataset/mitdb/csv'
@@ -67,29 +65,14 @@ signal_II_w = [ np.array([np.array([])]) for i in range(len(records))]
 classes = [[] for i in range(len(records))]
 R_poses = [[] for i in range(len(records))]
 selected_R = [np.array([]) for i in range(len(records))]
-temporal_features = [temp_features for i in range(len(records))]
+temporal_features = [temp_features() for i in range(len(records))]
 mit_db = mit_data()
 
 r_index = 0
 files = []
 patients = []
-for r in range(0,len(records),1):
-    print(r)
-    signal_II = []
-            
-records.sort()
-annotation_files.sort()
 
-list_classes = ['N', 'L', 'R', 'e', 'j', 'A', 'a', 'J', 'S', 'V', 'E', 'F', 'P', '/', 'f', 'u']
-
-# signal_II = [[] for i in range(len(records))]
-signal_II_w = [[] for i in range(len(records))]
-classes = [[] for i in range(len(records))]
-selected_R = [[] for i in range(len(records))]
-R_poses = [[] for i in range(len(records))]
-
-r_index = 0
-for r in range(0,len(records),1):
+for r in range(0,3,1):#records),1):
     
     signal_II = []
     print(r)
@@ -132,83 +115,7 @@ for r in range(0,len(records),1):
                 selected_R[r] = np.append(selected_R[r], 1)
             else:
                 selected_R[r] = np.append(selected_R[r], 0)
-        else:
-            selected_R[r] = np.append(selected_R[r], 0)
         R_poses[r].append(pos)
-                #signals{r} = [signals{r} data1(pos-window_size+1: pos+ window_size)];
-                signal_II_w[r] = signal_II[pos-window_size+1:pos+window_size]
-                # plt.plot(signal_II_w[r])
-                # plt.show()
-                classes[r].append(type)
-                selected_R[r].append(1)
-            else:
-                selected_R[r].append(0)
-        else:
-            selected_R[r].append(0)
-        
-        R_poses[r].append(pos)
-
-    # Compute RR Interval, feature Time
-    if(compute_RR_interval_feature):
-        pre_R = [0]
-        post_R = [R_poses[r][1] - R_poses[r][0]]
-        local_R = [] # Average of the ten past R intervals
-        global_R = [] # Average of the last 5 minutes of the signal
-        
-        for i in range(1,len(R_poses[r])-1, 1):
-            pre_R.insert(i, R_poses[r][i] - R_poses[r][i-1])
-            post_R.insert(i, R_poses[r][i+1] - R_poses[r][i])
-        
-        pre_R[0] = pre_R[1]
-        pre_R.append(R_poses[r][-1] - R_poses[r][-2])
-        post_R.append(post_R[-1])
-        
-        # Local R: AVG from past 10 RR intervals
-        for i in range(0,len(R_poses[r]), 1):
-            avg_val = 0
-            num_elems = 0
-            window = range(i-10,i,1)
-            
-            for w in window:
-                if w >= 0:
-                    avg_val = avg_val + pre_R[w]
-                    num_elems = num_elems + 1
-
-            if num_elems == 0:
-                local_R.append(0)   
-            else:
-                avg_val = avg_val / num_elems
-                local_R.append(avg_val)   
-        
-            # Global R: AVG from past 5 minutes
-            # 360 Hz  5 minutes = 108000 samples;
-            for i in range(1, len(R_poses[r]), 1):
-                avg_val = 0
-                back = -1
-                back_length = 0
-                if R_poses[r][i] < 108000:
-                    window = range(1,i,1)
-                else:
-                    while (i + back) > 0 and back_length < 108000:
-                        back_length = R_poses[r][i] - R_poses[r][i+back]
-                        back = back -1
-                    window = range(max(1,(back+i)), i, 1)
-                # Considerando distancia maxima hacia atras 
-                for w in window:
-                    avg_val = avg_val + pre_R[w]
-
-                avg_val = avg_val / len(window)
-                global_R.append(avg_val)
-        
-    #    %% Only keep those features from beats that we save list_classes
-    #    %% but for the computation of temporal features all the beats must be used
-    #    temporal_features{r}.pre_R = pre_R(selected_R{r} == 1);
-    #    temporal_features{r}.post_R = post_R(selected_R{r} == 1);
-    #    temporal_features{r}.local_R = local_R(selected_R{r} == 1);
-    #    temporal_features{r}.global_R = global_R(selected_R{r} == 1);
- 
-    # data_csv = 
-    # signal_II = 
 
     # Compute RR Interval, feature Time
     if(compute_RR_interval_feature):
@@ -289,6 +196,4 @@ output_path = '/local/scratch/mondejar/ECG/dataset/mitdb/m_learning/'
 # Protocol version 2 was introduced in Python 2.3. It provides much more efficient pickling of new-style classes.
 pickle.dump(mit_db, open(output_path + 'mit_db_' + str(window_size) + '.p', 'wb'), 2)
 
-# Load data
-#mit_db  = pickle.load(open('mit_db.p', 'rb'))
-# Create dataset
+
--- train_model_mitdb.py
+++ train_model_mitdb.py
@@ -1,1 +1,42 @@
-import tensorflow as tf
+""" 
+
+Author: Mondejar Guerra
+VARPA
+University of A Coruna
+April 2017
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import csv
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class temp_features:
+    def __init__(self):
+        # Instance atributes
+        self.pre_R = []
+        self.post_R = []
+        self.local_R = []
+        self.global_R = []
+
+class mit_data:
+    def __init__(self):
+        # Instance atributes
+        self.filenames = []
+        self.patients = []
+        self.signals = []
+        self.classes = []   
+        self.selected_R = []       
+        self.temporal_features = []       
+        self.window_size = []       
+
+window_size = 160
+
+db_path = '/local/scratch/mondejar/ECG/dataset/mitdb/m_learning/'
+
+# Load data
+mit_db  = pickle.load(open(db_path + 'mit_db_' + str(window_size) + '.p', 'rb'))
